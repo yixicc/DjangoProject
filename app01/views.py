64ac70105d79cc842ce3a14d04cd523d12b00404
huangyixi <huangyixi2013@163.com>
2025-07-21 20:31:11 +0800

    初始化项目

---
+++ app01/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render,HttpResponse
+
+# Create your views here.
+def index(request):
+    return HttpResponse("欢迎使用")
+
+def user_list(request):
+    return render(request,"user_list.html")
+
+def user_add(request):
+    return render(request,"user_add.html")
+
