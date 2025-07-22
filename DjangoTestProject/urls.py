515f9ef2a77f1ca295f7193d46e6d590dfc7981f
huangyixi <huangyixi2013@163.com>
2025-07-22 20:10:25 +0800

    login

--- DjangoTestProject/urls.py
+++ DjangoTestProject/urls.py
@@ -25,4 +25,9 @@ urlpatterns = [
        path('index/', views.index),
        path('user/list/', views.user_list),
        path('user/add/', views.user_add),
+       path('test/', views.test),
+
+       path('news/',views.news),
+       path('something/',views.something),
+       path('login/',views.login)
 ]
--- app01/views.py
+++ app01/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render,HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
+
 
 # Create your views here.
 def index(request):
@@ -10,3 +11,41 @@ def user_list(request):
 def user_add(request):
     return render(request,"user_add.html")
 
+def test(request):
+    name = "huangyixi"
+    role = ["CEO","Staff"]
+    userInfo = {"name":"huangxiaoming","salary":100000,"role":"cto"}
+    dataList =[
+        {"name": "zhangsan", "salary": 100000, "role": "staff"},
+        {"name": "lisi", "salary": 200000, "role": "staff"},
+        {"name": "wangwu", "salary": 300000, "role": "staff"}
+    ]
+    return render(request,"test.html",{"name":name,"role":role,"userInfo":userInfo,"dataList":dataList})
+
+def news(req):
+    import requests
+    res = requests.get("http://www.chinaunicom.com.cn/api/article/NewsByIndex/2/2025/7/news")
+    data_llist = res.json()
+    return render(req,"news.html",{"news_llist":data_llist})
+
+def something(request):
+    print("request.method : ",request.method)
+    print("request.path : ",request.path)
+    print("request.GET : ",request.GET)
+    print("request.POST : ",request.POST)
+
+    # return HttpResponse("返回内容")
+    # return render(request,"something.html")
+    return redirect("https://www.baidu.com")
+
+def login(request):
+    if request.method == "GET":
+        return render(request,"login.html")
+
+    username = request.POST.get("username")
+    password = request.POST.get("pwd")
+
+    if username == "root" and password == "123":
+        # return HttpResponse("登录成功！")
+        return redirect("https://www.baidu.com")
+    return render(request,"login.html",{"error_msg":"用户名或密码错误"})
