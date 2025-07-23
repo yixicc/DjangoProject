6187f2ad54bd22e939e0e7c347c98064079a596d
huangyixi <huangyixi2013@163.com>
2025-07-23 11:41:02 +0800

    连接数据库，创建表

--- DjangoTestProject/settings.py
+++ DjangoTestProject/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'DjangoTestProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'gx_day15',
+        'USER': 'root',
+        'PASSWORD': '123456',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
--- app01/models.py
+++ app01/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class UserInfo(models.Model):
+    name = models.CharField(max_length=20)
+    password = models.CharField(max_length=20)
+    age = models.IntegerField(default=3)
+    data = models.IntegerField(null=True,blank=True)
+
+class Department(models.Model):
+    title = models.CharField(max_length=20)
